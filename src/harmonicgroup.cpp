159f691e8524c3bcac4fdeffa3577aefb81e7b88
Gerald Mwangi <Geraldmwangi@gmx.de>
2022-05-15 07:53:12 +0200

    do not process harmonics higher then the highest fret

--- src/harmonicgroup.cpp
+++ src/harmonicgroup.cpp
@@ -2,21 +2,21 @@
 
 HarmonicGroup::HarmonicGroup()
 {
-    m_oldState=false;
+    m_oldState = false;
 }
 
 void HarmonicGroup::addNoteClassifier(shared_ptr<NoteClassifier> notecl)
 {
-    if(m_noteClassifiers.size()==0)
-        m_noteClassifiers.push_back(notecl); //Add fundamental
+    if (m_noteClassifiers.size() == 0)
+        m_noteClassifiers.push_back(notecl); // Add fundamental
     else
     {
-        for(int n=2;n<=12;n++)
+        for (int n = 2; n <= 12; n++)
         {
-            float min=m_noteClassifiers[0]->getCenterFrequency()*n-1;
-            float max=m_noteClassifiers[0]->getCenterFrequency()*n+1;
-            float f=notecl->getCenterFrequency();
-            if(f>=min&&f<=max)
+            float min = m_noteClassifiers[0]->getCenterFrequency() * n - 1;
+            float max = m_noteClassifiers[0]->getCenterFrequency() * n + 1;
+            float f = notecl->getCenterFrequency();
+            if (f >= min && f <= max)
                 m_noteClassifiers.push_back(notecl);
         }
     }
@@ -24,35 +24,38 @@ void HarmonicGroup::addNoteClassifier(shared_ptr<NoteClassifier> notecl)
 
 void HarmonicGroup::process(int nsamples)
 {
-    int numringing=0;
-    int numSamplesSinceLastOnset=m_noteClassifiers[0]->getNumSamplesSinceLastOnset();
-    //if(numSamplesSinceLastOnset>=0)
+    if (m_noteClassifiers[0]->getCenterFrequency() < 987.77)
     {
-        for (auto notecl : m_noteClassifiers)
+        int numringing = 0;
+        int numSamplesSinceLastOnset = m_noteClassifiers[0]->getNumSamplesSinceLastOnset();
+        // if(numSamplesSinceLastOnset>=0)
         {
-            // if(notecl!=m_noteClassifiers[0])
-            numringing += (notecl->is_ringing == true);
-            // numringing+=(notecl->getNumSamplesSinceLastOnset()>=0);
-            // if(abs(notecl->getNumSamplesSinceLastOnset()-numSamplesSinceLastOnset)<=3*nsamples&&notecl->is_ringing)
-            //     numringing++;
-        }
-        numringing*=m_noteClassifiers[0]->is_ringing;
-        if (numringing > 1)
-        {
-            if (!m_oldState)
+            for (auto notecl : m_noteClassifiers)
             {
-                m_noteClassifiers[0]->sendMidiNote(nsamples, true);
-                m_oldState = true;
-                // cout<<"Note on: "<<m_noteClassifiers[0]->getCenterFrequency()<<endl;
+                // if(notecl!=m_noteClassifiers[0])
+                numringing += (notecl->is_ringing == true);
+                // numringing+=(notecl->getNumSamplesSinceLastOnset()>=0);
+                // if(abs(notecl->getNumSamplesSinceLastOnset()-numSamplesSinceLastOnset)<=3*nsamples&&notecl->is_ringing)
+                //     numringing++;
             }
-        }
-        else
-        {
-            if (m_oldState)
+            numringing *= m_noteClassifiers[0]->is_ringing;
+            if (numringing > 1)
             {
-                m_noteClassifiers[0]->sendMidiNote(nsamples, false);
-                m_oldState = false;
-                // cout<<"Note off: "<<m_noteClassifiers[0]->getCenterFrequency()<<endl;
+                if (!m_oldState)
+                {
+                    m_noteClassifiers[0]->sendMidiNote(nsamples, true);
+                    m_oldState = true;
+                    // cout<<"Note on: "<<m_noteClassifiers[0]->getCenterFrequency()<<endl;
+                }
+            }
+            else
+            {
+                if (m_oldState)
+                {
+                    m_noteClassifiers[0]->sendMidiNote(nsamples, false);
+                    m_oldState = false;
+                    // cout<<"Note off: "<<m_noteClassifiers[0]->getCenterFrequency()<<endl;
+                }
             }
         }
     }
