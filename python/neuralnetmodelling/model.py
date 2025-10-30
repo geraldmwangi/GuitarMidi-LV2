185ae62ca16a19388828493995a83c3148c663c1
Gerald Mwangi <geraldmwangi@gmx.de>
2025-10-30 22:37:13 +0100

    started separable model

--- python/neuralnetmodelling/model.py
+++ python/neuralnetmodelling/model.py
@@ -4,19 +4,19 @@ def build_cnn_model(input_shape, output_dim,training=True):
     model = models.Sequential()
     model.add(layers.Input(shape=input_shape, dtype=tf.float32))
 
-    model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(layers.SeparableConv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu'))
     model.add(layers.BatchNormalization())
     if training:
         model.add(layers.SpatialDropout2D(0.2))
     model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(layers.SeparableConv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
     model.add(layers.BatchNormalization())
     if training:
         model.add(layers.SpatialDropout2D(0.2))
     model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(layers.SeparableConv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu'))
     if training:
         model.add(layers.BatchNormalization())
     model.add(layers.SpatialDropout2D(0.3))
