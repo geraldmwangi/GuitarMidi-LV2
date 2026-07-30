fb782f9008d7f2918ba1c409d79ec2c2be4b521b
Gerald Mwangi <geraldmwangi@gmx.de>
2026-07-30 18:37:58 +0200

    reverted to master

--- src/noteinferencer.cpp
+++ src/noteinferencer.cpp
@@ -50,8 +50,7 @@ namespace GuitarMidi
             {
                 // check if all harmonics of the note are active before sending note on message
                 float note_energy = 0;
-                int h = 1; // 
-                //for (int h = 1; h <= NUM_HARMONICS; h++)
+                int h = 1; // for (int h = 1; h <= NUM_HARMONICS; h++)
                 {
                     int harmonic_index = i * NUM_HARMONICS + h - 1;
                     if (harmonic_index < NUM_HARMONICS * NUM_NOTES)
@@ -67,8 +66,8 @@ namespace GuitarMidi
                 smoothed_onsetoutput[i] = *m_smoothing * smoothed_onsetoutput[i] + (1 - *m_smoothing) * output_data[i]; // simple low pass filter to smooth the output and reduce jitter
                 smoothed_offsetoutput[i] = *m_smoothing_offset * smoothed_offsetoutput[i] + (1 - *m_smoothing_offset) * output_data[i];
 
-                smoothed_noteenergies[i] = *m_smoothing * smoothed_noteenergies[i] + (1 - *m_smoothing) * note_energy ;//* smoothed_onsetoutput[i]; // smooth the note energy to avoid jitter
-                smoothed_offsetnoteenergies[i] = *m_smoothing_offset * smoothed_offsetnoteenergies[i] + (1 - *m_smoothing_offset) * note_energy;// * smoothed_offsetoutput[i];
+                smoothed_noteenergies[i] = *m_smoothing * smoothed_noteenergies[i] + (1 - *m_smoothing) * note_energy * smoothed_onsetoutput[i]; // smooth the note energy to avoid jitter
+                smoothed_offsetnoteenergies[i] = *m_smoothing_offset * smoothed_offsetnoteenergies[i] + (1 - *m_smoothing_offset) * note_energy * smoothed_offsetoutput[i];
                 if (smoothed_onsetoutput[i] > *m_onset_threshold)
                 {
 
@@ -121,7 +120,7 @@ namespace GuitarMidi
                         continue;
                     }
                     // lv2_log_note(&g_logger, "Note %d OFF with confidence %f\n", i, output_data[i]);
-                    if (m_note_on[i] && (smoothed_offsetoutput[i] < *m_offset_threshold) && (i != (NUM_NOTES - 1)))
+                    if (m_note_on[i] && smoothed_offsetoutput[i] < *m_offset_threshold && i != (NUM_NOTES - 1))
                     {
                         uint8_t midinote[3] = {0x90, i + NOTE_OFFSET, 0x00};
                         m_midioutput.sendMidiMessage(midinote, 0);
