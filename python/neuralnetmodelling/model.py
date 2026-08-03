0c4a3c45afc5497d02d85798615bafd1276add7c
Gerald Mwangi <geraldmwangi@gmx.de>
2026-08-03 15:16:42 +0200

    training with no chord layers: epoch 5  val_bin_acc: 0.9418 - val_f1_score: 0.4578 - val_loss: 0.3373 - val_precision: 0.5151 - val_recall: 0.4119

--- python/neuralnetmodelling/model.py
+++ python/neuralnetmodelling/model.py
@@ -225,16 +225,17 @@ def chord_conv_block(string_features, filters,output_dim,training, kernel_size=(
     stacked = layers.Concatenate(axis=1, name=f"{name_prefix}_stack_strings")(expanded)  # (B, 6, max_len, 64)
 
 
-    c1=layers.Conv2D(filters, kernel_size, padding='same', name=f"{name_prefix}_conv1",kernel_regularizer=reg2d)(stacked)
-    c1=layers.BatchNormalization(name=f"{name_prefix}_bn1")(c1)
-    c1 = layers.ELU(name=f"{name_prefix}_act1")(c1)
+    # c1=layers.Conv2D(filters, kernel_size, padding='same', name=f"{name_prefix}_conv1",kernel_regularizer=reg2d)(stacked)
+    # c1=layers.BatchNormalization(name=f"{name_prefix}_bn1")(c1)
+    # c1 = layers.ELU(name=f"{name_prefix}_act1")(c1)
 
 
-    c2=layers.Conv2D(2*filters, kernel_size, padding='same', name=f"{name_prefix}_conv2",kernel_regularizer=reg2d)(c1)
-    c2=layers.BatchNormalization(name=f"{name_prefix}_bn2")(c2)
-    c2=layers.LeakyReLU(name=f"{name_prefix}_act2")(c2)
-    chord=c2#layers.Add(name=f"{name_prefix}_res_c1_c2")([c2, c1])
-    chord=layers.SpatialDropout2D(0.2, name=f"{name_prefix}_drop1")(chord)
+    # c2=layers.Conv2D(2*filters, kernel_size, padding='same', name=f"{name_prefix}_conv2",kernel_regularizer=reg2d)(c1)
+    # c2=layers.BatchNormalization(name=f"{name_prefix}_bn2")(c2)
+    # c2=layers.LeakyReLU(name=f"{name_prefix}_act2")(c2)
+    # chord=c2#layers.Add(name=f"{name_prefix}_res_c1_c2")([c2, c1])
+    # chord=layers.SpatialDropout2D(0.2, name=f"{name_prefix}_drop1")(chord)
+    chord=stacked
     # Split the chord features back into per-string tensors
     split_chords = [layers.Lambda(lambda t, i=i: t[:, i, :, :], name=f"{name_prefix}_slice_str{i}")(chord) for i in range(len(string_features))]
     
