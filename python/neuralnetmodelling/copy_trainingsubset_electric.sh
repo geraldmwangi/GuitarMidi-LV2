16ad8c41c2bc5f13a1274fe025aeef64cd8835a2
Gerald Mwangi <geraldmwangi@gmx.de>
2026-08-03 23:45:11 +0200

    testing guitarmidi_smooth_chord_dist_epoch43_valAcc0.9684_valPrec0.7585_valRecall0.6801.keras

--- python/neuralnetmodelling/copy_trainingsubset_electric.sh
+++ python/neuralnetmodelling/copy_trainingsubset_electric.sh
@@ -8,6 +8,6 @@ USEDRECORDS_FILE="usedrecords-electric.txt"
 REMOVE_SOURCE_FILES=false # Set to true if you want to move instead of copy
 REMOVE_TARGET_FILES=false # Set to true if you want to delete files from target after copying (use with caution!)
 RETAIN_TARGET_FILES_PERCENTAGE=40 # Percentage of files to retain in target when REMOVE_TARGET_FILES is true (0-100)
-NUM_FILES_TO_COPY=1000 #24300 #5690 #20000
+NUM_FILES_TO_COPY=24300 #5690 #20000
 
 ./copy_trainingsubset.sh "$SOURCE_DIR" "$TARGET_DIR" "$USEDRECORDS_FILE" "$REMOVE_SOURCE_FILES" "$REMOVE_TARGET_FILES" "$RETAIN_TARGET_FILES_PERCENTAGE" "$NUM_FILES_TO_COPY"
