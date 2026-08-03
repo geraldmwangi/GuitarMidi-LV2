47ee1861701be7a4c07c68db79bb094c156dd4c2
Gerald Mwangi <geraldmwangi@gmx.de>
2026-08-04 00:32:34 +0200

    bump

--- python/neuralnetmodelling/copy_trainingsubset_electric.sh
+++ python/neuralnetmodelling/copy_trainingsubset_electric.sh
@@ -6,7 +6,7 @@ SOURCE_DIR="/data/training_subset_electric/"
 TARGET_DIR="/training_cache/training_subset_electric"
 USEDRECORDS_FILE="usedrecords-electric.txt"
 REMOVE_SOURCE_FILES=false # Set to true if you want to move instead of copy
-REMOVE_TARGET_FILES=false # Set to true if you want to delete files from target after copying (use with caution!)
+REMOVE_TARGET_FILES=true # Set to true if you want to delete files from target after copying (use with caution!)
 RETAIN_TARGET_FILES_PERCENTAGE=40 # Percentage of files to retain in target when REMOVE_TARGET_FILES is true (0-100)
 NUM_FILES_TO_COPY=24300 #5690 #20000
 
